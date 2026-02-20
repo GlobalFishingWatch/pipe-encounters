f32a53d764d2e46aee91c0ab8ca94794391a7a4e
Tomás Link <tomas.link@globalfishingwatch.org>
2026-02-20 13:28:07 -0300

    Add requirements.txt compiled with pip tools

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ from setuptools import setup
 setup(
     name='encounters',
     version='4.3.2',
-    packages=find_packages(exclude=['test*.*', 'tests'])
+    packages=find_packages(exclude=['test*.*', 'tests']),
+    install_requires=[
+        "apache-beam[gcp]~=2.49",
+        "more_itertools~=8.12",
+        "s2sphere~=0.2",
+        "statistics~=1.0.3",
+    ]
 )
